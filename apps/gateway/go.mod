0ac5f1a476bcc1bc0bec0960de14a91f62aa4c9b
Loui Lu <junhaol1206@gmail.com>
2026-05-05 12:42:41 -0700

    feat: add gateway config for control plane and Redis

--- apps/gateway/go.mod
+++ apps/gateway/go.mod
@@ -2,4 +2,9 @@ module platform/gateway
 
 go 1.23
 
-require github.com/go-chi/chi/v5 v5.2.0
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/redis/go-redis/v9 v9.19.0
+)
+
+require github.com/cespare/xxhash/v2 v2.3.0 // indirect
--- apps/gateway/internal/config/config.go
+++ apps/gateway/internal/config/config.go
@@ -3,14 +3,16 @@ package config
 import "os"
 
 type Config struct {
-	Port             string
-	SampleBackendURL string
+	Port            string
+	ControlPlaneURL string
+	RedisURL        string
 }
 
 func Load() Config {
 	return Config{
-		Port:             getenv("GATEWAY_PORT", "8080"),
-		SampleBackendURL: getenv("SAMPLE_BACKEND_URL", "http://sample-backend:6060"),
+		Port:            getenv("GATEWAY_PORT", "8080"),
+		ControlPlaneURL: getenv("CONTROL_PLANE_URL", "http://control-plane:4000"),
+		RedisURL:        getenv("REDIS_URL", "redis://redis:6379"),
 	}
 }
 
